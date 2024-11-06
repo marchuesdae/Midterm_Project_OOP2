253523b620f51d7bb89e4e75a10e05e11da1c9c0
March <154644649+marchuesdae@users.noreply.github.com>
2024-11-06 23:25:55 +0800

    Add files via upload

---
+++ Employee.php
@@ -0,0 +1,13 @@
+<?php
+    class Employee{
+        private $companyName;
+
+        public function __construct($companyName){
+            $this->setCompanyname($companyName);
+        }
+
+        public function setCompanyname($companyName){
+            
+        }
+    }
+?>
