257d8f595179caaf38f274d80cb6552c0f83a2b1
March <154644649+marchuesdae@users.noreply.github.com>
2024-11-08 03:55:09 +0800

    Person.php

--- Person.php
+++ Person.php
@@ -1,56 +1,51 @@
 <?php
-    class Person{
-        private $name, $address, $age;
+class Person {
+    private $name, $address, $age;
 
-        public function __construct($name, $address, $age)
-        {
-            $this->setName($name);
-            $this->setAddress($address);
-            $this->setAge($age);
-        }
-
-        public function setName($name){
-            if(is_string($name)){
-                $this->name = $name;
-            }
-            else{
-                echo"Invalid name...";
-            }
-        }
-
-        public function getName(){
-            return $this->name;
-        }
-
-        public function setAddress($address){
-            if(is_string($address)){
-                $this->address = $address;
-            }
-            else{
-                echo"Invalid address...";
-            }
-        }
-
-        public function getAddress(){
-            return $this->address;
-        }
-
-        public function setAge($age){
-            if(is_numeric($age)){
-                $this->age = $age;
-            }
-        }
-
-        public function getAge(){
-            echo"Invalid age...";
-        }
-
-        public function __toString($name, $age, $address)
-        {
-            echo"Name: " . $this->name . "\nAge : " . $this->age . "\nAddress : " . $this->address;
-        }
-
-        
-
+    public function __construct($name, $address, $age) {
+        $this->name = $name;
+        $this->address = $address;
+        $this->age = $age;
     }
+
+    public function setName($name) {
+        if (is_string($name)) {
+            $this->name = $name;
+        } else {
+            echo "Invalid name...\n";
+        }
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setAddress($address) {
+        if (is_string($address)) {
+            $this->address = $address;
+        } else {
+            echo "Invalid address...\n";
+        }
+    }
+
+    public function getAddress() {
+        return $this->address;
+    }
+
+    public function setAge($age) {
+        if (is_numeric($age) && $age > 0) {
+            $this->age = $age;
+        } else {
+            echo "Invalid age...\n";
+        }
+    }
+
+    public function getAge() {
+        return $this->age;
+    }
+
+    public function __toString() {
+        return "Name: " . $this->name . "\nAge: " . $this->age . "\nAddress: " . $this->address . "\n";
+    }
+}
 ?>
